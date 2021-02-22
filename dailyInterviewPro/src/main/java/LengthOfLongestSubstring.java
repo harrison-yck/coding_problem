ad78d02b00419c503e35dd859693a28a789a01f4
harrisonyuen <yck971213@outlook.com>
2021-02-23 00:09:29 +0800

    add daily question

---
+++ dailyInterviewPro/src/main/java/LengthOfLongestSubstring.java
@@ -0,0 +1,29 @@
+/*
+    Given a string, find the length of the longest substring without repeating characters.
+
+    Example:
+    Input: 'abrkaabcdefghijjxxx'
+    Output: 10
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LengthOfLongestSubstring {
+    static int solve(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        int longest = 0;
+        int offset = 0;
+        int length = s.length();
+
+        for (int i = 0; i < length; i++) {
+            if (map.containsKey(s.charAt(i))) offset = Math.max(offset, map.get(s.charAt(i)) + 1);
+
+            map.put(s.charAt(i), i);
+            longest = Math.max(longest, i - offset + 1);
+        }
+
+        return longest;
+    }
+}
---
+++ dailyInterviewPro/src/test/java/LengthOfLongestSubstringTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LengthOfLongestSubstringTest {
+
+    @Test
+    void solve() {
+        assertThat(LengthOfLongestSubstring.solve("abrkaabcdefghijjxxx")).isEqualTo(10);
+        assertThat(LengthOfLongestSubstring.solve("abcabcbb")).isEqualTo(3);
+        assertThat(LengthOfLongestSubstring.solve("bbbbb")).isEqualTo(1);
+        assertThat(LengthOfLongestSubstring.solve("pwwkew")).isEqualTo(3);
+        assertThat(LengthOfLongestSubstring.solve("")).isEqualTo(0);
+        assertThat(LengthOfLongestSubstring.solve(" ")).isEqualTo(1);
+        assertThat(LengthOfLongestSubstring.solve("dvdf")).isEqualTo(3);
+    }
+}
