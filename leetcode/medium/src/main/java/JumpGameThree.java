757bbcabb9a55802522124a1377cdf408fa1c366
harrisonyuen <harrison.pinnacle@gmail.com>
2021-02-24 16:35:38 +0800

    refactor jump game three with better approach for dp

--- leetcode/medium/src/main/java/JumpGameThree.java
+++ leetcode/medium/src/main/java/JumpGameThree.java
@@ -12,10 +12,30 @@
 */
 
 public class JumpGameThree {
-    static boolean[] VISITED;
+//    static boolean[] VISITED;
+//
+//    static boolean canReach(int[] arr, int start) {
+//        VISITED = new boolean[arr.length];
+//        return traversal(arr, start);
+//    }
+//
+//    static boolean traversal(int[] arr, int index) {
+//        if (index < 0 || index >= arr.length) return false;
+//
+//        if (arr[index] == 0) {
+//            return true;
+//        } else if (VISITED[index]) { // visited -> reach again -> loop
+//            return false;
+//        }
+//
+//        VISITED[index] = true;
+//
+//        return traversal(arr, index + arr[index]) || traversal(arr, index - arr[index]);
+//    }
 
+
+    // Better approach -> use -1 to represent visited
     static boolean canReach(int[] arr, int start) {
-        VISITED = new boolean[arr.length];
         return traversal(arr, start);
     }
 
@@ -24,12 +44,13 @@ public class JumpGameThree {
 
         if (arr[index] == 0) {
             return true;
-        } else if (VISITED[index]) { // visited -> reach again -> loop
+        } else if (arr[index] == -1) { // visited -> reach again -> loop
             return false;
         }
 
-        VISITED[index] = true;
+        int steppedInt = arr[index];
+        arr[index] = -1;
 
-        return traversal(arr, index + arr[index]) || traversal(arr, index - arr[index]);
+        return traversal(arr, index + steppedInt) || traversal(arr, index - steppedInt);
     }
 }
