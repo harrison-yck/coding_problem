09f567e0fcbfc26e53916032baae1b4361994d57
harrisonyuen <harrison.pinnacle@gmail.com>
2021-03-12 13:48:45 +0800

    add coin change topdown

--- leetcode/medium/src/main/java/CoinChange.java
+++ leetcode/medium/src/main/java/CoinChange.java
@@ -1,49 +1,48 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * @author harrison
  */
 public class CoinChange {
-    private static int[] dp;
-
-    public static int iterativeTopDown(int[] coins, int target) {
-        dp = new int[target + 1];
+    public static int iterativeBottomUp(int[] coins, int target) {
+        int[] dp = new int[target + 1];
 
         for (int amount = 1; amount <= target; amount++) { // option
             dp[amount] = Integer.MAX_VALUE;
             for (int coin : coins) {
                 int nextAmount = amount - coin;
-                if (nextAmount < 0) continue;
+                if (nextAmount < 0) continue; // base
 
-                if (dp[nextAmount] != Integer.MAX_VALUE) dp[amount] = Math.min(dp[amount], 1 + dp[nextAmount]); // state = nextAmount
+                if (dp[nextAmount] != Integer.MAX_VALUE) dp[amount] = Math.min(dp[amount], 1 + dp[nextAmount]); // state = amount -> nextAmount
             }
         }
 
         return dp[target] == Integer.MAX_VALUE ? -1 : dp[target];
     }
 
-//    public static int recursiveTopDown(int[] coins, int amount) {
-//        dp = new int[amount + 1];
-//        return topDown(coins, amount);
-//    }
-//
-//    // state = current
-//    private static int topDown(int[] coins, int current) {
-//        if (dp[current] > 0) return dp[current];
-//
-//        // base
-//        if (current == 0) return 0;
-//
-//        // option
-//        int min = Integer.MAX_VALUE;
-//        for (int coin : coins) {
-//            int subProblem = current - coin;
-//            if (subProblem < 0) continue;
-//
-//            min = Math.min(min, 1 + topDown(coins, subProblem));
-//        }
-//        dp[current] = min == Integer.MAX_VALUE ? -1 : min;
-//
-//        return dp[current];
-//    }
+    public static int recursiveTopDown(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        return topDown(coins, amount, dp);
+    }
+
+    private static int topDown(int[] coins, int current, int[] dp) {
+        // base
+        if (current == 0) return 0;
+        if (current < 0) return -1;
+
+        if (dp[current] != 0) return dp[current];
+
+        // option
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int subProblem = topDown(coins, current - coin, dp);
+
+            // state transit
+            if (subProblem >= 0 && subProblem < min) min = 1 + subProblem;
+        }
+        dp[current] = min == Integer.MAX_VALUE ? -1 : min;
+
+        return dp[current];
+    }
 }
--- leetcode/medium/src/test/java/CoinChangeTest.java
+++ leetcode/medium/src/test/java/CoinChangeTest.java
@@ -5,11 +5,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CoinChangeTest {
 
     @Test
-    void iterativeBottomUp() {
-        assertThat(CoinChange.iterativeTopDown(new int[]{1, 2, 5}, 11)).isEqualTo(3);
-        assertThat(CoinChange.iterativeTopDown(new int[]{2}, 3)).isEqualTo(-1);
+    void iterativeTopDown() {
+        assertThat(CoinChange.iterativeBottomUp(new int[]{1, 2, 5}, 11)).isEqualTo(3);
+        assertThat(CoinChange.iterativeBottomUp(new int[]{2}, 3)).isEqualTo(-1);
+        assertThat(CoinChange.iterativeBottomUp(new int[]{1}, 0)).isEqualTo(0);
+        assertThat(CoinChange.iterativeBottomUp(new int[]{1}, 1)).isEqualTo(1);
+        assertThat(CoinChange.iterativeBottomUp(new int[]{1}, 2)).isEqualTo(2);
+        assertThat(CoinChange.iterativeBottomUp(new int[]{186, 419, 83, 408}, 6249)).isEqualTo(20);
+    }
 
+    @Test
+    void recursiveTopDown() {
         assertThat(CoinChange.recursiveTopDown(new int[]{1, 2, 5}, 11)).isEqualTo(3);
         assertThat(CoinChange.recursiveTopDown(new int[]{2}, 3)).isEqualTo(-1);
+        assertThat(CoinChange.recursiveTopDown(new int[]{1}, 0)).isEqualTo(0);
+        assertThat(CoinChange.recursiveTopDown(new int[]{1}, 1)).isEqualTo(1);
+        assertThat(CoinChange.recursiveTopDown(new int[]{1}, 2)).isEqualTo(2);
+        assertThat(CoinChange.recursiveTopDown(new int[]{186, 419, 83, 408}, 6249)).isEqualTo(20);
     }
 }
