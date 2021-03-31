7692b59675deec50b3ed4866c912a701bfb4395d
harrisonyuen <yck971213@outlook.com>
2021-04-01 00:18:44 +0800

    daily

--- leetcode/hard/src/main/java/StampingTheSequence.java
+++ leetcode/hard/src/main/java/StampingTheSequence.java
@@ -1,47 +1,64 @@
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Queue;
-import java.util.Set;
 
 /**
  * @author harrison
  */
 public class StampingTheSequence {
-    static Deque<Integer> result = new ArrayDeque<>();
-
-    public static int[] movesToStamp(String stamp, String target) {
+    //
+    public int[] movesToStamp(String stamp, String target) {
+        int targetLength = target.length();
+        int stampLength = stamp.length();
         char[] stampArr = stamp.toCharArray();
         char[] targetArr = target.toCharArray();
 
-        int numOfReplacedChar = 0;
+        int replaced = 0;
+        boolean[] visited = new boolean[targetLength];
+        List<Integer> result = new ArrayList<>();
 
-        while (target.length() != numOfReplacedChar) {
-            int index = target.indexOf(stamp);
-            result.push(index);
-            replace(targetArr, stampArr, index);
-            if (index == -1) break;
+        outer:
+        while (true) {
+            boolean updateAny = false;
+            // valid solution should update something for every traversal
+            for (int i = 0; i <= targetLength - stampLength; i++) {
+                // use visited to avoid checking already replaced range
+                if (!visited[i] && canReplace(targetArr, stampArr, i)) {
+                    result.add(i);
+                    replaced += replace(targetArr, stampArr, i);
+                    visited[i] = true;
+                    updateAny = true;
+
+                    if (replaced == targetLength) break outer; // find valid solution
+                }
+            }
+
+            if (!updateAny) return new int[0]; // no solution
         }
 
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        // I used ArrayDeque originally. But use list and then change to array is faster (7ms -> 3ms)
+        int[] res = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            res[i] = result.get(result.size() - i - 1);
+        }
+
+        return res;
     }
 
-    private static void replace(char[] currentArr, char[] stampArr, int index) {
+    private boolean canReplace(char[] targetArr, char[] stampArr, int i) {
+        for (int stampIndex = 0; stampIndex < stampArr.length; stampIndex++) {
+            if (targetArr[stampIndex + i] != '?' && targetArr[stampIndex + i] != stampArr[stampIndex]) return false; // match stamp pattern / wildcard
+        }
+        return true;
+    }
+
+    private int replace(char[] currentArr, char[] stampArr, int index) {
+        int replaceCount = 0;
         for (int i = index, stampIndex = 0; i < index + stampArr.length; i++, stampIndex++) {
-            currentArr[i] = '*';
+            if (currentArr[i] != '?') {
+                currentArr[i] = '?';
+                replaceCount++;
+            }
         }
-    }
-
-    private static boolean canReplace(char[] currentArr, char[] stampArr, int index) {
-        int replaceTo = index + stampArr.length;
-
-        for (int replaceFrom = index; replaceFrom <= replaceTo; replaceFrom++) {
-
-        }
-
-        return false;
+        return replaceCount;
     }
 }
--- leetcode/hard/src/test/java/StampingTheSequenceTest.java
+++ leetcode/hard/src/test/java/StampingTheSequenceTest.java
@@ -8,6 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class StampingTheSequenceTest {
     @Test
     void test() {
-        assertThat(StampingTheSequence.movesToStamp("abc", "ababc")).isEqualTo(new int[]{0, 2});
+        var stampingTheSequence = new StampingTheSequence();
+        assertThat(stampingTheSequence.movesToStamp("abc", "ababc")).isEqualTo(new int[]{0, 2});
+        assertThat(stampingTheSequence.movesToStamp("abca", "aabcaca")).isIn(new int[]{0, 3, 1}, new int[]{3, 0, 1});
+        assertThat(stampingTheSequence.movesToStamp("aye", "eyeye")).isEqualTo(new int[]{});
+        assertThat(stampingTheSequence.movesToStamp("h", "hhhhh")).isEqualTo(new int[]{4, 3, 2, 1, 0});
+        assertThat(stampingTheSequence.movesToStamp("mda", "mdadddaaaa")).isEqualTo(new int[]{});
     }
 }
