ce1fa20bb08c78b282fedfae76eeefa8078b242c
harrisonyuen <yck971213@outlook.com>
2021-04-12 23:52:38 +0800

    faster way to count deepest sum

--- leetcode/medium/src/main/java/DeepestLeavesSum.java
+++ leetcode/medium/src/main/java/DeepestLeavesSum.java
@@ -7,16 +7,39 @@ import java.util.Map;
  * @author harrison
  */
 public class DeepestLeavesSum {
+//    public int deepestLeavesSum(TreeNode root) {
+//        Map<Integer, Integer> levelSum = new HashMap<>();
+//        int maxDepth = dfs(root, 1, levelSum);
+//        return levelSum.get(maxDepth);
+//    }
+//
+//    private int dfs(TreeNode node, int level, Map<Integer, Integer> levelSum) {
+//        if (node == null) return level - 1;
+//
+//        levelSum.put(level, levelSum.getOrDefault(level, 0) + node.val);
+//        return Math.max(dfs(node.left, level + 1, levelSum), dfs(node.right, level + 1, levelSum));
+//    }
+
+    private int maxLevel = 0;
+    private int sum = 0;
+
     public int deepestLeavesSum(TreeNode root) {
-        Map<Integer, Integer> levelSum = new HashMap<>();
-        int maxDepth = dfs(root, 1, levelSum);
-        return levelSum.get(maxDepth);
+        if (root == null) return 0;
+        dfs(root, 0);
+        return sum;
     }
 
-    private int dfs(TreeNode node, int level, Map<Integer, Integer> levelSum) {
-        if (node == null) return level - 1;
+    private void dfs(TreeNode node, int level) {
+        if (node == null) return;
 
-        levelSum.put(level, levelSum.getOrDefault(level, 0) + node.val);
-        return Math.max(dfs(node.left, level + 1, levelSum), dfs(node.right, level + 1, levelSum));
+        if (level > maxLevel) {
+            sum = node.val;
+            maxLevel = level;
+        } else if (level == maxLevel) {
+            sum += node.val;
+        }
+
+        dfs(node.left, level + 1);
+        dfs(node.right, level + 1);
     }
 }
--- leetcode/medium/src/test/java/DeepestLeavesSumTest.java
+++ leetcode/medium/src/test/java/DeepestLeavesSumTest.java
@@ -7,7 +7,7 @@ class DeepestLeavesSumTest {
     @Test
     void test() {
         DeepestLeavesSum deepestLeavesSum = new DeepestLeavesSum();
-        var tree = TreeNode.create(1, 2, 3, 4, 5, null, 6, 7, null, null, null, null, 8);
+        TreeNode tree = TreeNode.create(1, 2, 3, 4, 5, null, 6, 7, null, null, null, null, null, null, 8);
         assertThat(deepestLeavesSum.deepestLeavesSum(tree)).isEqualTo(15);
     }
 }
