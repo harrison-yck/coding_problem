1bdb8b91a6ed6e3d82233362322872e291b52f52
harrisonyuen <yck971213@outlook.com>
2021-04-17 01:08:19 +0800

    daily

--- leetcode/medium/src/main/java/RemoveAllAdjacentDuplicatesInStringII.java
+++ leetcode/medium/src/main/java/RemoveAllAdjacentDuplicatesInStringII.java
@@ -1,27 +1,34 @@
+import java.util.Arrays;
+
 /**
  * @author harrison
  */
 public class RemoveAllAdjacentDuplicatesInStringII {
     public String removeDuplicates(String s, int k) {
-        int[] count = new int[26];
-        int[] countB = new int[26];
+        int dupCount = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) == s.charAt(i)) dupCount++;
+            else dupCount = 1;
 
-        for (char c : s.toCharArray()) {
-            int index = c - 'a';
-            count[index]++;
+            if (dupCount == k) s = removeDuplicates(s.substring(0, i - k + 1) + s.substring(i + 1), k);
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            int index = c - 'a';
-            if (count[index] % k != 0) {
-                countB[index]++;
-
-                if (count[i])
-                sb.append(c);
-                count[index]--;
-            }
-        }
-        return sb.toString();
+        return s;
     }
+
+// lee215 approach
+// using two pointer to keep track of previous and current char
+// move i back until that char is less than k
+
+//    public String removeDuplicates(String s, int k) {
+//        int i = 0, n = s.length();
+//
+//        int[] count = new int[n];
+//        char[] stack = s.toCharArray();
+//        for (int j = 0; j < n; i++, j++) {
+//            stack[i] = stack[j];
+//            count[i] = i > 0 && stack[i - 1] == stack[j] ? count[i - 1] + 1 : 1;
+//            if (count[i] == k) i -= k;
+//        }
+//        return new String(stack, 0, i);
+//    }
 }
