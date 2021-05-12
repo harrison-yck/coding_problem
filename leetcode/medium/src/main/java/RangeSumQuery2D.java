6fe0609ea8415c3b6f882e388ec62c91bc003239
harrisonyuen <harrison.pinnacle@gmail.com>
2021-05-12 18:00:20 +0800

    daily

---
+++ leetcode/medium/src/main/java/RangeSumQuery2D.java
@@ -0,0 +1,21 @@
+/**
+ * @author harrison
+ */
+public class RangeSumQuery2D {
+    private final int[][] sumMatrix;
+
+    public RangeSumQuery2D(int[][] matrix) {
+        int x = matrix.length, y = matrix[0].length;
+        sumMatrix = new int[x + 1][y + 1];
+
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= y; j++) {
+                sumMatrix[i][j] = sumMatrix[i - 1][j] + sumMatrix[i][j - 1] - sumMatrix[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sumMatrix[row2 + 1][col2 + 1] - sumMatrix[row2 + 1][col1] - sumMatrix[row1][col2 + 1] + sumMatrix[row1][col1];
+    }
+}
--- leetcode/medium/src/main/java/SetMatrixZeros.java
+++ leetcode/medium/src/main/java/SetMatrixZeros.java
@@ -21,4 +21,42 @@ public class SetMatrixZeros {
             }
         }
     }
+
+    public void setZeroesOne(int[][] matrix) { // O(1) space
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        boolean rowZero = false;
+        boolean colZero = false;
+
+        for (int i = 0; i < cols; i++)
+            if (matrix[0][i] == 0) {
+                rowZero = true;
+                break;
+            }
+
+        for (int i = 0; i < rows; i++)
+            if (matrix[i][0] == 0) {
+                colZero = true;
+                break;
+            }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < matrix[0].length; i++) if (rowZero) matrix[0][i] = 0;
+        for (int i = 0; i < matrix.length; i++) if (colZero) matrix[i][0] = 0;
+    }
 }
