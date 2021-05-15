fd75f24876e962db4830f6f9d75605a0fede516e
harrison <harrison_yck@outlook.com>
2021-05-15 23:33:02 +0800

    a normal version

--- leetcode/hard/src/main/java/ValidNumber.java
+++ leetcode/hard/src/main/java/ValidNumber.java
@@ -1,5 +1,3 @@
-import java.util.regex.Pattern;
-
 /**
  * @author harrison
  */
@@ -11,12 +9,40 @@ public class ValidNumber {
 
             char lastChar = s.charAt(length - 1);
             int lastCharToInt = lastChar - '0';
-            if (lastChar != '.' && (lastCharToInt < 0 || lastCharToInt > 9)) return false; // last char must be an integer
+            if (lastChar != '.' && (lastCharToInt < 0 || lastCharToInt > 9))
+                return false; // last char must be an integer
 
             Double.parseDouble(s);
             return true;
         } catch (NumberFormatException ex) {
             return false;
         }
+    }
+
+    public boolean isNumberV2(String s) {
+        boolean hasDot = false;
+        boolean hasE = false;
+        boolean hasNumber = false;
+        short signCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                hasNumber = true;
+            } else if (c == 'e' || c == 'E') {
+                if (hasE || !hasNumber) return false;
+                hasE = true;
+                hasNumber = false;
+            } else if (c == '.') {
+                if (hasE || hasDot) return false;
+                hasDot = true;
+            } else if (c == '+' || c == '-') {
+                if (signCount >= 2 || i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
+                signCount++;
+            } else {
+                return false;
+            }
+        }
+        return hasNumber;
     }
 }
