d13f92ed203e2e75f91fe007505c9062b6c47d3c
harrison <harrison_yck@outlook.com>
2021-05-15 22:40:18 +0800

    add iterative

--- leetcode/medium/src/main/java/FlattenBinaryTree.java
+++ leetcode/medium/src/main/java/FlattenBinaryTree.java
@@ -6,14 +6,45 @@ import datastructure.TreeNode;
 public class FlattenBinaryTree {
     private TreeNode prev = null;
 
-    public void flatten(TreeNode root) {
+    public void postOrderRecur(TreeNode root) {
         if (root == null) return;
 
-        flatten(root.right);
-        flatten(root.left);
+        postOrderRecur(root.right);
+        postOrderRecur(root.left);
 
         root.right = prev;
         root.left = null;
         prev = root;
+    }
+
+    public void preOrderRecur(TreeNode root) {
+        if (root == null) return;
+
+        preOrderRecur(root.left);
+        preOrderRecur(root.right);
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        root.left = null;
+        root.right = left;
+        while (root.right != null) root = root.right;
+        root.right = right;
+    }
+
+    public void preOrderIter(TreeNode root) {
+        while (root != null) {
+            if (root.left == null) {
+                root = root.right;
+            } else {
+                TreeNode left = root.left;
+                while (left.right != null) {
+                    left = left.right;
+                }
+                left.right = root.right;
+                root.right = root.left;
+                root.left = null;
+            }
+        }
     }
 }
