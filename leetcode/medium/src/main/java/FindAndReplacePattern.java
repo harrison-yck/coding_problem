49ed1e36dd4357af3a150208ea30f8419d4ae17e
harrisonyuen <harrison.pinnacle@gmail.com>
2021-05-21 17:32:57 +0800

    refactor

--- leetcode/medium/src/main/java/FindAndReplacePattern.java
+++ leetcode/medium/src/main/java/FindAndReplacePattern.java
@@ -6,36 +6,35 @@ import java.util.List;
  */
 public class FindAndReplacePattern {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
-        List<String> samePattern = new ArrayList<>();
-
         char[] patternChars = pattern.toCharArray();
-        for (String s : words) {
-            char[] map = new char[26];
-            boolean[] used = new boolean[26];
-            boolean valid = true;
-
-            char[] wordChars = s.toCharArray();
-            for (int i = 0; i < patternChars.length; i++) {
-                char p = patternChars[i];
-                char value = map[p - 'a'];
-
-                if (value == '\0') {
-                    if (used[wordChars[i] - 'a']) {
-                        valid = false;
-                        break;
-                    }
-                    map[p - 'a'] = wordChars[i];
-                    used[wordChars[i] - 'a'] = true;
-                } else {
-                    if (value != wordChars[i]) {
-                        valid = false;
-                        break;
-                    }
-                }
-            }
-            if (valid) samePattern.add(s);
-        }
-
+        List<String> samePattern = new ArrayList<>();
+        for (String word : words) if (isValid(word, patternChars)) samePattern.add(word);
         return samePattern;
+    }
+
+    private boolean isValid(String word, char[] patternChars) {
+        char[] map = new char[26];
+        int used = 0;
+
+        char[] wordChars = word.toCharArray();
+        for (int i = 0; i < patternChars.length; i++) {
+            char p = patternChars[i];
+            char c = wordChars[i];
+            char value = map[p - 'a'];
+
+            // hit but not equal
+            if (value != 0 && value != c) return false;
+
+            // not hit
+            if (value == 0) {
+                int location = 1 << (c - 'a');
+
+                // used
+                if ((used & location) != 0) return false;
+                map[p - 'a'] = c;
+                used |= location; // set value
+            }
+        }
+        return true;
     }
 }
