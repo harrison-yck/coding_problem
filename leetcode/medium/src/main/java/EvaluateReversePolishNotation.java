0d89b72b327fdb45ac7aeaf8d002bae99c5cd721
harrisonyuen <harrison.pinnacle@gmail.com>
2021-05-25 15:53:52 +0800

    daily

---
+++ leetcode/medium/src/main/java/EvaluateReversePolishNotation.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+/**
+ * @author harrison
+ */
+public class EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        int[] stack = new int[tokens.length / 2 + 1];
+        int current = 0;
+
+        for (String s: tokens) {
+            int d;
+            switch (s) {
+                case "+" -> {
+                    int a = stack[current--];
+                    int b = stack[current--];
+                    d = a + b;
+                }
+                case "-" -> {
+                    int a = stack[current--];
+                    int b = stack[current--];
+                    d = a - b;
+                }
+                case "*" -> {
+                    int a = stack[current--];
+                    int b = stack[current--];
+                    d = a * b;
+                }
+                case "/" -> {
+                    int a = stack[current--];
+                    int b = stack[current--];
+                    d = a / b;
+                }
+                default -> d = Integer.parseInt(s);
+            }
+            stack[current++] = d;
+        }
+
+        return stack[0];
+    }
+}
