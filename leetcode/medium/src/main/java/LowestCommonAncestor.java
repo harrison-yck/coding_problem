a7a0b21a834c55dcc4dbf7c4af3ae2732cd77225
harrisonyuen <harrison.pinnacle@gmail.com>
2021-06-30 18:05:38 +0800

    daily

---
+++ leetcode/medium/src/main/java/LowestCommonAncestor.java
@@ -0,0 +1,22 @@
+import datastructure.TreeNode;
+
+/**
+ * @author harrison
+ */
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        if (left != null) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+}
